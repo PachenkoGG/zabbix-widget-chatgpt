1e23a26d1953992e3931ba4214168947859690bb
Pachenko <pachenko@github.com>
2026-02-15 20:39:44 +0300

    Fix: Correct syntax error in widget.view.php

--- views/widget.view.php
+++ views/widget.view.php
@@ -16,44 +16,40 @@ use Modules\OpenAIAssistant\Services\WidgetTranslator;
         // Hidden input to pass Zabbix data to JavaScript
         (new CInput('hidden', 'widget-zabbix-data'))
             ->setAttribute('value', $data['zabbix_data'] ?? '')
-            ->setId('widget-zabbix-data')
-        ,
+            ->setId('widget-zabbix-data'),
+        
         (new CDiv([
             (new CDiv([
                 (new CTag('h3', true, 'OpenAI Assistant'))
-                    ->addStyle('margin: 0; color: #fff; font-size: 1.2rem;')
-                ,
+                    ->addStyle('margin: 0; color: #fff; font-size: 1.2rem;'),
+                
                 (new CButton('clear-button', WidgetTranslator::translate('view.clear.button')))
                     ->addClass('chat-clear-button')
                     ->setAttribute('type', 'button')
-                ,    
             ]))
-                ->addClass('chat-header')
-            ,
+                ->addClass('chat-header'),
+            
             (new CDiv())
                 ->setId('chat-log')
-                ->addClass('chat-log')
-            ,
+                ->addClass('chat-log'),
+            
             (new CDiv([
                 (new CInput('text'))
                     ->addClass('chat-form-message')
-                    ->setAttribute('placeholder', WidgetTranslator::translate('view.message.placeholder'))
-                ,
+                    ->setAttribute('placeholder', WidgetTranslator::translate('view.message.placeholder')),
+                
                 (new CButton('stop-button', '◼'))
                     ->addClass('chat-form-button-stop')
                     ->addClass('chat-form-button-stop--hidden')
-                    ->setAttribute('type', 'button')
-                ,
+                    ->setAttribute('type', 'button'),
+                
                 (new CButton('send-button', '➜'))
                     ->addClass('chat-form-button')
                     ->setAttribute('type', 'submit')
-                ,
-                ]))
+            ]))
                 ->addClass('chat-form')
-            ,
         ]))
-        ->setId('chat-container')
-        ->addClass('chat-container')
-    )
-->show();
-
+            ->setId('chat-container')
+            ->addClass('chat-container')
+    ])
+    ->show();
